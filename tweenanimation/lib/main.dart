08e99784c84ca6ecd33242f593b7550daaab6f64
sinanex <ms4730435@gmail.com>
2025-01-16 19:35:00 +0530

    animated container

--- tweenanimation/lib/main.dart
+++ tweenanimation/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tweenanimation/container.dart';
 import 'package:tweenanimation/listpage.dart';
+import 'package:tweenanimation/opacity.dart';
 
 void main(){
    runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedConatinerPage(),
+      home: AnimatedOpaPAge(),
     );
   }
 }
---
+++ tweenanimation/lib/opacity.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOpaPAge extends StatefulWidget {
+  const AnimatedOpaPAge({super.key});
+
+  @override
+  State<AnimatedOpaPAge> createState() => _AnimatedOpaPAgeState();
+}
+
+class _AnimatedOpaPAgeState extends State<AnimatedOpaPAge> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GestureDetector(
+          onTap: () {
+            setState(() {
+              opacity = opacity == 1.0 ? 0.0 : 1.0;
+            });
+          },
+          child: AnimatedOpacity(
+            opacity: opacity,
+            duration: Duration(seconds: 3),
+            child: Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+            ),
+          )),
+    );
+  }
+}
+
+double opacity = 1;
